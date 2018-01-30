070a0996f796298c98022a65928fcd5175654c25
Ellie Richards <eleanor4.richards@live.uwe.ac.uk>
2018-01-30 00:47:14 +0000

    Basic motion

--- Assets/2DScripts/PlayerMovement2D.cs
+++ Assets/2DScripts/PlayerMovement2D.cs
@@ -9,9 +9,17 @@ public class PlayerMovement2D : MonoBehaviour
 
     private Rigidbody2D rig;
 
+    [Range(0.5f, 5.0f)]
     public float fallMultiplier = 2.5f;
-    public float lowJumpMultiplier = 2.0f;
+
+    [Range(0.5f, 5.0f)]
     public float jumpVelocity = 5.0f;
+
+    [Range(0.1f, 2.0f)]
+    public float walkMultiplier = 5.0f;
+
+    [SerializeField]
+    private Vector2 moveDirection;
 
     // Use this for initialization
     private void Start()
@@ -24,6 +32,12 @@ public class PlayerMovement2D : MonoBehaviour
     {
         TakeInput();
 
+        ApplyMotion();
+    }
+
+    private void ApplyMotion()
+    {
+        //slam down jump
         if (rig.velocity.y < 0)
         {
             rig.AddForce(Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime, ForceMode2D.Impulse);
@@ -32,14 +46,22 @@ public class PlayerMovement2D : MonoBehaviour
         {
             rig.AddForce(Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime, ForceMode2D.Impulse);
         }
+        if (!IsGrounded())
+        {
+            //horizontal movement
+            rig.AddForce(moveDirection * walkMultiplier, ForceMode2D.Impulse);
+        }
     }
 
     private void TakeInput()
     {
+        //Jump Input
         if (Input.GetButtonDown("Jump"))
         {
             rig.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
         }
+
+        moveDirection = new Vector2((Input.GetAxis("Horizontal")), 0);
     }
 
     private bool IsGrounded()
